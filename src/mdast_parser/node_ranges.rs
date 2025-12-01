d0014c221ba8a4a26fc029acb65c8b4e394290ab
JohnCLlStokes <johnclstokes@hotmail.com>
2025-12-01 23:10:06 +0000

    ive added an iterator i may not use? do i need to spend a bit longer specifying this before i start building lol

--- src/mdast_parser/node_ranges.rs
+++ src/mdast_parser/node_ranges.rs
@@ -1,5 +1,4 @@
 // in this file we will:
-// define a means of pushing new node ranges to the struct
 // define a means of merging adjacent ranges
 use enum_map::EnumMap;
 use crate::mdast_parser::NodeType;
@@ -25,8 +24,45 @@ impl NodeRanges {
     }
 
     /// add range to the heap
-    pub fn add() {}
+    /// do we assume start is before end or should i enforce that here?
+    pub fn add(&mut self, kind: NodeType, start: u32, end: u32) {
+        self.ranges[kind].push(
+            NodeStartEnd(start, end)
+        )
+    }
+
+    /// get the vect for a given node type
+    /// as a borrowed iterable rather than rawdogging the accessor
+    /// optionally bound the iterator with a given start / end period
+    pub fn get_ranges(
+        &self,
+        kind: NodeType,
+        start_at: Option<u32>,
+        end_at: Option<u32>,
+    ) -> impl Iterator<Item = &NodeStartEnd> {
+        let ranges = &self.ranges[kind];
+
+        // begin iterating only after start is no longer less than the selected threshold
+        let start_idx = if let Some(start_threshold) = start_at {
+            ranges.partition_point(|r| r.start < start_threshold)
+        } else {
+            0
+        };
+
+        ranges[start_idx..]
+            .iter()
+            .take_while(move |r| {
+                if let Some(end_threshold) = end_at {
+                    r.end <= threshold  // break out once we're too begin
+                } else {
+                    true
+                }
+            })
+    }
 
     /// merge adjacent or overlapping ranges of a shared type
-    pub fn merge_ranges() {}
+    /// this will stop us over calculating later
+    /// does it belong here or somewhere else?
+    /// when should it be called to minimise the API width?
+    pub fn deduplicate_ranges() {}
 }
