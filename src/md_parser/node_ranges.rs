c1b4bb97d8e2ba434959b6a8f355c11739fda7bc
JohnCLlStokes <johnclstokes@hotmail.com>
2026-01-18 18:19:14 +0000

    minor tweaks to formatting to keep to consistant structure

--- src/md_parser/node_ranges.rs
+++ src/md_parser/node_ranges.rs
@@ -1,20 +1,6 @@
 use std::fmt;
 use enum_map::EnumMap;
-use crate::md_parser::NodeType;
-
-/// the start and end of the range as identified in the algo
-#[derive(Clone, Copy)]
-pub struct NodeStartEnd {
-    pub start: u32,  // are these big enough? how do we handle the ambiguity?????
-    pub end: u32,
-}
-
-/// allow users to easily view the range
-impl fmt::Display for NodeStartEnd {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "[{}, {}]", self.start, self.end)
-    }
-}
+use crate::md_parser::{NodeType, NodeStartEnd};
 
 /// HashMap Enum hybrid thing i found for representing
 /// every range for a given NodeType
