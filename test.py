0cee2c5ff24fb2adf1c9c96e2228c7b44a6f8de3
JohnCLlStokes <johnclstokes@hotmail.com>
2026-03-08 16:05:51 +0000

    proof of overlap working git status

--- test.py
+++ test.py
@@ -3,4 +3,4 @@ from darn_it import Chunker
 with open("README.md", "r") as f:
     readme = f.read()
 
-print(Chunker().get_chunks(readme, 500, "tokens"))
+print(Chunker().get_chunks(readme, 500, "tokens", overlap=50))
