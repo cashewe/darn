32ccc888fbb8e205ee203bde6f6dbcb8b73cdbdc
JohnCLlStokes <johnclstokes@hotmail.com>
2026-05-04 18:52:26 +0000

    mama oh mama can i please let this work....

--- src/python_objects/rule.rs
+++ src/python_objects/rule.rs
@@ -1,5 +1,7 @@
 use pyo3::prelude::*;
 use crate::python_objects::PyNodeType;
+use crate::rule_manager::Rule as BackendRule;
+use crate::punishments::lookup_punishment;
 
 /// the Rule defines the punishment we will enact upon our poor text
 #[pyclass]
@@ -19,4 +21,36 @@ pub struct Rule {
 
     #[pyo3(get)]
     pub off_scale: usize,
+}
+
+
+/// convert a python rule into a rust one
+impl TryFrom<&Rule> for BackendRule {
+    type Error = PyErr;
+
+    fn try_from(r: &Rule) -> PyResult<Self> {
+        let on_fn = lookup_punishment(&r.on_punishment)
+            .ok_or_else(|| pyo3::exceptions::PyValueError::new_err(
+                format!(
+                    "Unknown on_punishment '{}'. Valid options: const, linear, inverse_triangular, reverse_linear",
+                    r.on_punishment
+                )
+            ))?;
+
+        let off_fn = lookup_punishment(&r.off_punishment)
+            .ok_or_else(|| pyo3::exceptions::PyValueError::new_err(
+                format!(
+                    "Unknown off_punishment '{}'.",
+                    r.off_punishment
+                )
+            ))?;
+
+        Ok(BackendRule {
+            node_type:      r.nodetype.into(),
+            on_punishment:  on_fn,
+            on_scale:       r.on_scale,
+            off_punishment: off_fn,
+            off_scale:      r.off_scale,
+        })
+    }
 }
