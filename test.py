9f89d6adf26c54c589b37e76ede7212416649f05
JohnCLlStokes <johnclstokes@hotmail.com>
2026-05-04 18:55:38 +0000

    trying rules out in the python file

--- test.py
+++ test.py
@@ -1,6 +1,21 @@
-from darn_it import Chunker
+from darn_it import (
+    Chunker,
+    NodeType,
+    Rule,
+)
 
 with open("README.md", "r") as f:
     readme = f.read()
 
 print(Chunker().get_chunks(readme, 500, "tokens", overlap=50))
+
+rules = [
+    Rule(
+        on_punishment="const",
+        on_scale=10,
+        off_punishment="const",
+        off_scale=0,
+        nodetype=NodeType.Sentence
+    )
+]
+print(Chunker(rules).get_chunks(readme, 500, "tokens", overlap=50))
